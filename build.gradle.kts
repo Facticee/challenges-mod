fac36d0273b4843d6b96b7d306fc12153f73f849
Facticee <timon.eliah.plum@gmail.com>
2026-06-13 21:39:10 +0200

    test

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 plugins {
     id("net.fabricmc.fabric-loom")
     id("org.jetbrains.kotlin.jvm") version "2.4.0"
+    id("com.gradleup.shadow") version "9.2.2"
 }
 
 version = providers.gradleProperty("mod_version").get()
@@ -12,6 +13,16 @@ dependencies {
     implementation("net.fabricmc:fabric-loader:${providers.gradleProperty("loader_version").get()}")
     implementation("net.fabricmc.fabric-api:fabric-api:${providers.gradleProperty("fabric_api_version").get()}")
     implementation("net.fabricmc:fabric-language-kotlin:${providers.gradleProperty("fabric_kotlin_version").get()}")
+
+    shadow("org.jetbrains.kotlin:kotlin-stdlib")
+    shadow("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+}
+
+tasks.shadowJar {
+    configurations = listOf(project.configurations.shadow.get())
+    mergeServiceFiles()
+
+    relocate("kotlin", "io.github.facticee.shadow.kotlin")
 }
 
 tasks.processResources {
